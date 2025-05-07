e4df677d8a6da5cee075f179203d82f648565666
egwolk <erincovacha@gmail.com>
2025-05-07 12:32:45 +0800

    Add: display logic to show "No records found" message in logbook page

--- logbook.php
+++ logbook.php
@@ -31,23 +31,29 @@ $past_checkins = searchTable($pdo, 'logbook', ['name'], 'checkout_time IS NOT NU
             <th>Check-out</th>
             <th>Delete</th>
         </tr>
-        <?php foreach ($past_checkins as $c):
-            $checkin = new DateTimeImmutable($c['checkin_time']);
-            $checkout = new DateTimeImmutable($c['checkout_time']);
-        ?>
+        <?php if (empty($past_checkins)): ?>
             <tr>
-                <td><?= htmlspecialchars($c['name']) ?></td>
-                <td><?= $checkin->format('m-d-Y') ?></td> <!-- mm-dd-yyyy -->
-                <td><?= $checkin->format('h:i A') ?></td> <!-- hh:mm AM/PM -->
-                <td><?= $checkout->format('h:i A') ?></td>
-                <td>
-                    <button
-                        onclick="if(confirm('Delete this record?')) window.location.href='delete_checkin.php?id=<?= $c['id'] ?>&from=logbook'">
-                        Delete
-                    </button>
-                </td>
+                <td colspan="8" style="text-align: center;">No records found</td>
             </tr>
-        <?php endforeach; ?>
+        <?php else: ?>
+            <?php foreach ($past_checkins as $c):
+                $checkin = new DateTimeImmutable($c['checkin_time']);
+                $checkout = new DateTimeImmutable($c['checkout_time']);
+            ?>
+                <tr>
+                    <td><?= htmlspecialchars($c['name']) ?></td>
+                    <td><?= $checkin->format('m-d-Y') ?></td> <!-- mm-dd-yyyy -->
+                    <td><?= $checkin->format('h:i A') ?></td> <!-- hh:mm AM/PM -->
+                    <td><?= $checkout->format('h:i A') ?></td>
+                    <td>
+                        <button
+                            onclick="if(confirm('Delete this record?')) window.location.href='delete_checkin.php?id=<?= $c['id'] ?>&from=logbook'">
+                            Delete
+                        </button>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        <?php endif; ?>
     </table>
 
     <br><a href="index.php">Back to Main Page</a>
